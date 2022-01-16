a74a6b49ce8b76e334c7d3a7ef7f58a78ed8612b
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-16 14:35:37 -0600

    feat(Root): Comienza a trabajar en el sidebar

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,24 +1,36 @@
 import { useState, useRef } from 'react'
+import VideoPlayerActions from './VideoPlayerActions'
 import styles from './styles.module.css'
-const videoSRC = 'https://v16-webapp.tiktok.com/a837eadd3d9f4baebfcad6ba16f80eeb/61e48f04/video/tos/useast2a/tos-useast2a-ve-0068c001/f17241e832a6457d84e55c5db0ccde5a/?a=1988&br=1250&bt=625&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FRTkag3-I&l=20220116153155010189066023275C3E95&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anVnbzw6ZmhoODMzNzczM0ApOWY7aDM2M2UzN2lnNzc0NWdfYTBecjRfb3NgLS1kMTZzc14wMjItYjIvLi1iLTUzYGA6Yw%3D%3D&vl=&vr='
+import clsx from 'clsx'
 
-export default function VideoPlayer () {
+export default function VideoPlayer ({ src }) {
   const [playing, setPlaying] = useState(false)
-  const video = useRef()
+  const video = useRef(null)
 
   const handlePlay = () => {
-    if (playing) {
-      video.current.pause()
-    } else {
-      video.current.play()
-    }
+    const { current: videoEl } = video
+
+    playing
+      ? videoEl.pause()
+      : videoEl.play()
+
     setPlaying(!playing)
   }
-
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing
+  })
   return (
-    <div>
-      <video ref={video} className={styles.video} src={videoSRC} controls={false} />
-      <button onClick={handlePlay} className={styles.player} />
+    <div className={styles.wrapper}>
+      <video
+        ref={video}
+        className={styles.video}
+        src={src}
+        loop
+        controls={false}
+        onClick={handlePlay}
+      />
+      <button onClick={handlePlay} className={playerClassName} />
+      <VideoPlayerActions />
     </div>
   )
 }
