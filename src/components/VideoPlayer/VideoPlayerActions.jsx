9f6a5330d9a87eb4aac2b4d21222a37edde1ae30
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-16 15:26:27 -0600

    feat(Root): Usa los nuevos Iconos

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,22 @@
 import { Heart } from '../Icons/Heart'
+import { Share } from '../Icons/Share'
+import { Comment } from '../Icons/Comment'
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({ likes = 2102, comments = 645, shares = 440, headted = false }) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='like'>{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title='comments'>{comments}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='shares'>{shares}</strong>
       </div>
     </aside>
   )
