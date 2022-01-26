5b13f9f8c597c907bd1bf5af5da0624fc24da785
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-25 18:05:18 -0600

    feat(Root): Agrega la funcion de publicar el video

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,37 @@
 import { supabase } from './supabase'
 
+const prefix = import.meta.env.VITE_SUPABASE_STORAGE_URL
+export const uploadVideo = async ({ videoFile }) => {
+  const filename = window.crypto.randomUUID()
+
+  const { data, error } = await supabase.storage
+    .from('videos')
+    .upload(`uploads/${filename}.mp4`, videoFile)
+
+  const file = data?.Key ? `${prefix}${data.Key}` : ''
+
+  return [error, file]
+}
+
+export const publishVideo = async ({ videoSrc, description }) => {
+  const defaultAlbum = 'https://cdn.worldvectorlogo.com/logos/react-1.svg'
+  const defaultSong = 'Never gonna give you up'
+
+  const { data, error } = await supabase
+    .from('videos')
+    .insert([
+      {
+        user_id: 'b83db538-58b8-4727-b5fe-227218098b31',
+        description,
+        albumCover: defaultAlbum,
+        songTitle: defaultSong,
+        src: videoSrc
+      }
+    ])
+
+  return [error, data]
+}
+
 export const getVideos = async () => {
   const { data, error } = await supabase
     .from('videos')
@@ -9,6 +41,7 @@ export const getVideos = async () => {
         id
     )
     `)
+    .order('created_at', { ascending: false })
   if (error) return [error]
 
   return [error, data]
